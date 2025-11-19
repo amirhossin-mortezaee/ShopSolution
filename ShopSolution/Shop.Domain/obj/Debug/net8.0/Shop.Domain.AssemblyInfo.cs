b0d93f75abf13ddd9de98fff94340d45dffa91f5
AmirHossin Mortezaee <129367158+amirhossin-mortezaee@users.noreply.github.com>
2025-11-19 08:54:10 +0330

    feat(domain): update BaseEntity Id to Guid and initialize with Guid.NewGuid()

--- ShopSolution/Shop.Domain/obj/Debug/net8.0/Shop.Domain.AssemblyInfo.cs
+++ ShopSolution/Shop.Domain/obj/Debug/net8.0/Shop.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shop.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+036f8ba0ec6be3b8e08d6bd2c9ff27bc59b64889")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shop.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shop.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
