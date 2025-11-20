c3a8d2d666842676811ad32a7be4a011106518cd
AmirHossin Mortezaee <129367158+amirhossin-mortezaee@users.noreply.github.com>
2025-11-20 12:25:25 +0330

    feat(domain): add base properties to Product aggregate

--- ShopSolution/Shop.Domain/Entities/ProductAgg/Product.cs
+++ ShopSolution/Shop.Domain/Entities/ProductAgg/Product.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OnlineShop.Domain.Commons;
+using Shop.Domain.Commons;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,13 @@ using System.Threading.Tasks;
 
 namespace Shop.Domain.Entities.ProductAgg
 {
-    internal class Product
+    public class Product : BaseEntity , IAggregateRoot
     {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public int Stock { get; private set; }
+        public Guid CategoryId { get; private set; }
+        public bool IsActive { get; private set; }
     }
 }
